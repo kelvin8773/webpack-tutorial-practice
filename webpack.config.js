682abf39b42877d9b14eb876987ab5c5921be52f
Kelvin Liang <kelvin9877@gmail.com>
2019-12-05 22:05:59 +0800

    add babel into project

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,20 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower-components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          },
+        },
+      },
+    ],
+  },
 };
 
