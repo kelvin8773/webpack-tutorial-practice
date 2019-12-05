d450039b1e3c4bf20afd01582dc5e010e7b3bd7d
Kelvin Liang <kelvin9877@gmail.com>
2019-12-05 20:50:51 +0800

    clear up for asset-management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon-check.png';
-import Data from './dataset.xml';
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello! ', 'Webpack is wonderful Tool!!!'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myImage = new Image();
-  myImage.src = Icon;
-
-  element.appendChild(myImage);
-
-  console.log(Data);
 
   return element;
 }
